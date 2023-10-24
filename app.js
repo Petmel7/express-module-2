57c171ac43f85da20631d28e656aac672c1642e2
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-24 19:01:58 +0300

    add

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import exppress from "express";
 import { movies } from "./movies.js";
+import moment from "moment";
+import fs from "fs/promises";
 
 const app = exppress();
 
-app.use((req, res) => {
-    console.log("First middeware")
+app.use(async (req, res, next) => {
+    const { method, url } = req;
+    const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+    await fs.appendFile("./public/server.log", `\n${method}, ${url}, ${date}`);
+    next();
 })
+
+// app.use((req, res, next) => {
+//     console.log("First middeware");
+//     next();
+// })
 
 app.get("/products", (req, res) => {
     res.json([]);
@@ -15,4 +25,4 @@ app.get("/movies", (req, res) => {
     res.json(movies);
 })
 
-app.listen(3010, () => console.log("Server running at 3010 port"));
+app.listen(3011, () => console.log("Server running at 3011 port"));
